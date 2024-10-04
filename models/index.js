38a59475f570835d767f0044138aba2ccc572897
Minh Khanh Pham <minhkhanh.pham@tuni.fi>
2024-10-05 00:58:30 +0300

    delete unnecessary files

--- models/index.js
+++
@@ -1,6 +0,0 @@
-import Level from "./levelModel.js";
-import Gameplay from "./gameplayModel.js";
-import User from "./userModel.js";
-import Frame from "./frameModel.js";
-
-export { Level, Gameplay, User, Frame };
