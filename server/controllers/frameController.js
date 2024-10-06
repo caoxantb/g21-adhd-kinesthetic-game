609c4f58cca18066da770e317a2f2fbfde9a1df0
Minh Khanh Pham <minhkhanh.pham@tuni.fi>
2024-10-07 00:02:37 +0300

    fix frame controller

--- server/controllers/frameController.js
+++ server/controllers/frameController.js
@@ -22,11 +22,10 @@ export const getAllFramesForLevel = async (req, res) => {
     const { levelId } = req.params;
     const level = await Level.findById(levelId); 
     if (!level) throw new NotFound("Level does not exist"); // if the level does not exist in the database
-    else {
-        const frames = await Frame.find({ difficultyLevel: { $in: [levelId] } }); 
-        if (frames.length === 0) {
-            throw new NotFound("No frames found"); // if the level does not have any frames
-        }
-        res.status(200).json(frames); 
+    const frames = await Frame.find({ difficultyLevel: { $in: [levelId] } }); 
+    if (frames.length === 0) {
+        throw new NotFound("No frames found"); // if the level does not have any frames
     }
+    res.status(200).json(frames); 
+
 };
