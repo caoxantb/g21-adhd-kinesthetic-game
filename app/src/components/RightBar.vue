b4d33e0fe1320db8b71f08a8e967efc4539ba0e0
wuannan <annan@wuannan.com>
2024-11-16 16:43:09 +0200

    fix

--- app/src/components/RightBar.vue
+++ app/src/components/RightBar.vue
@@ -48,7 +48,7 @@ function triggerJump() {
 
     <img class="skeleton" :src="skeleton" alt="" />
 
-    <el-button size="large" @click="triggerJump">Jump</el-button>
+    <!-- <el-button size="large" @click="triggerJump">Jump</el-button> -->
   </div>
 </template>
 
